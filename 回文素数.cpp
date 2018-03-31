bd727c48917c854b4328309498110117bffd8678
nijigenirubasho <1597232528@qq.com>
2018-03-31 21:04:22 +0800

    Create 回文素数.cpp

---
+++ 回文素数.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input_num;
+    cout<<"palindromic primes <= ";
+    cin>>input_num;
+    int pp_count=0;
+    for(int each=2; each<=input_num; each++)
+    {
+        int factorization_list=0;
+        for(int factor=1; factor<=each; factor++)
+            if(each%factor==0&&!(factor>each/factor))
+                factorization_list++;
+        if(factorization_list==1)
+        {
+            int antitone=0,each_cpy=each;
+            while(each_cpy)
+            {
+                antitone=antitone*10+each_cpy%10;
+                each_cpy/=10;
+            }
+            if(antitone==each)
+            {
+                pp_count++;
+                cout<<pp_count<<':'<<each<<endl;
+            }
+        }
+    }
+    return 0;
+}
